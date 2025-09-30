6ca3f91990a9654c533776cccad5d923cd1415af
IgorCostaIfrn <costa.igor@escolar.ifrn.edu.br>
2025-09-30 08:20:19 -0300

    aula 30/09, implementando a criação e a consulta

--- todo-list/todo-list2.php
+++ todo-list/todo-list2.php
@@ -1,51 +1,67 @@
-<?php 
-
-$localhost = 'localhost'; 
+<?php
+# conectando o banco
+$localhost = 'localhost';
 $usario = 'root';
 $senha = '';
-$database = 'todo_list'; 
+$database = 'todo_list';
 
-$conn = mysqli_connect($localhost,$usario,$senha, $database);
+$conn = new mysqli($localhost, $usario, $senha, $database);
 
-if($conn->connect_error){
-    die('Deu erro na conexão'. $conn->connect_error);
+# se a conecsão der errado
+if ($conn->connect_error) {
+    die('Deu erro na conexão' . $conn->connect_error);
 }
 
 # criacao de tarefas
-if(isset($_POST['descricao'])){
+if (isset($_POST['descricao']) && !empty(trim($_POST['descricao']))) {
     $descricao = $conn->real_escape_string($_POST['descricao']);
-    $sqlInsert = "INSERT INTO tarefas (descricao) VALUES ('$descricao')"; 
+    $sqlInsert = "INSERT INTO tarefas (descricao) VALUES ('$descricao')";
+    if ($conn->query($sqlInsert) === TRUE) {
+        header("location: todo-list2.php");
+    }
 }
 # Exclusão de tarefas
 
 
-$tarefas=[]; 
+$tarefas = [];
 # Listar tarefas
-
-
+$sqlSelect = "SELECT * FROM tarefas ORDER BY data_criacao DESC";
+$result = $conn->query($sqlSelect);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $tarefas[] = $row;
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo-list</title>
 </head>
+
 <body>
 
     <h1>TO-DO List</h1>
     <form action="todo-list2.php" method="POST">
-        <input type="text" placeholder="Descrição da sua tarefa" name="descricao"/>
-        <button type="submit">Adicionar</button> 
+        <input type="text" placeholder="Descrição da sua tarefa" name="descricao" />
+        <button type="submit">Adicionar</button>
     </form>
 
-    <h2>Suas tarefas</h2>
-    <?php if(!empty($tarefas)):?>
-    <h3>Suas tarefas</h3>
-    <?php else:?>
-    <h3>Não tem tarefas</h3>
-    <?php endif;?>
+    <?php if (!empty($tarefas)): ?>
+        <h3>Suas tarefas</h3>
+        <ol>
+            <?php foreach ($tarefas as $tarefa): ?>
+                <li><?= htmlspecialchars($tarefa['descricao']) ?> (<?= $tarefa['data_criacao'] ?>)</li>
+            <?php endforeach; ?>
+        </ol>
+    <?php else: ?>
+        <h3>Não tem tarefas</h3>
+    <?php endif; ?>
 
 </body>
+
 </html>
