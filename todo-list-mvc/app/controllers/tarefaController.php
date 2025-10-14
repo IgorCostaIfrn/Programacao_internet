5e1cc577ee80eeb63c3086657822ba4d6f452eb9
IgorCostaIfrn <costa.igor@escolar.ifrn.edu.br>
2025-10-14 08:22:31 -0300

    aula 14/10, subindo a view listar e index

--- todo-list-mvc/app/controllers/tarefaController.php
+++ todo-list-mvc/app/controllers/tarefaController.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/../models/Tarefa.php";
+require_once __DIR__ . "/../models/tarefa.php";
 class TarefaController
 {
 
@@ -10,7 +10,14 @@ class TarefaController
         $this->tarefaModel = new Tarefa();
     }
 
-    #   CRIAR
+    #   LISTAR 
+    public function index()
+    {
+        $tarefas =  $this->tarefaModel->listar();
+        include __DIR__ . "/../views/listar.php";
+    }
+
+    #   CRIAR 
     public function criar()
     {
         if (isset($_POST['descricao']) && !empty(trim($_POST['descricao']))) {
@@ -23,11 +30,13 @@ class TarefaController
     #   EXCLUIR 
     public function excluir()
     {
-        if (isset($_POST['delete'])) {
-            $this->tarefaModel->excluir($_POST['delete']);
+        if (isset($_GET['delete'])) {
+            $this->tarefaModel->excluir($_GET['delete']);
         }
 
-        header("location: index.php"); 
+        header("location: index.php");
     }
+
+
 }
 ?>
